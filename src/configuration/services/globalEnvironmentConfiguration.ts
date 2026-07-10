87326f8ecd3ad9f99a410edbb55870d631afa924
Marnix Dessing <m.dessing@nijmegen.nl>
2026-07-10 11:57:54 +0200

    docs: describe enterprise search password parameter

--- src/configuration/services/globalEnvironmentConfiguration.ts
+++ src/configuration/services/globalEnvironmentConfiguration.ts
@@ -4,9 +4,14 @@ import { Statics } from '../../Statics';
 
 export const ENTERPRISE_SEARCH_ENGINE = 'kiss-engine';
 
+/**
+ * This parameter is filled manually by connecting to the EC2 instance
+ * and following the steps in src/elasticsearch/get-enterprise-search-password.sh
+ * TODO figure out how this should be done in prod.
+ */
 export const ENTERPRISE_SEARCH_PRIVATE_API_KEY = new AppParameter({
   type: 'secret',
   id: 'kcc-enterprise-search-private-api-key',
-  description: 'Enterprise Search private API key (used by kiss, sync, and ES constructs)',
+  description: 'Enterprise Search private API key (used by kiss, sync, and ES constructs). Note: this should be set manually, see IaC for docs.',
   path: `/${Statics.projectName}/kiss/elastic/enterprise-search-private-api-key-shared`,
 });
